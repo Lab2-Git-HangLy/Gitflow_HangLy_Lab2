e158d9bf1dbe989d8ac9cc0079e1fd28659947dd
Hang Ly <Hanglyu2002@gmail.com>
2022-10-05 13:36:05 +0300

    Added submodule

--- calculator.py
+++ calculator.py
@@ -3,10 +3,10 @@ b = int(input("Enter second number:"))
 c = input("Enter choice:")
 
 if (c == 1):
-    print a + b
+    print (a + b)
 if (c == 2):
-    print a - b
+    print (a - b)
 if (c == 3):
-    print a * b
+    print (a * b)
 if (c == 4):
-    print a / b
+    print (a / b)
